9a93c7cf20776cac6ba07adfc0b15936ccd214b7
Benjamin Gleitzman <gleitz@mit.edu>
2018-03-23 10:53:12 -0700

    Fix bash powerline. Few small os preferences edits

--- ansible/roles/bash/files/.ap-bash-powerline.sh
+++ ansible/roles/bash/files/.ap-bash-powerline.sh
@@ -12,61 +12,20 @@ __powerline() {
     COLOR_SUCCESS='\[$(tput setaf 2)\]' # green
     COLOR_FAILURE='\[$(tput setaf 1)\]' # red
 
-    SYMBOL_GIT_BRANCH='⑂'
+    SYMBOL_GIT_BRANCH='\342\[\221\202\]' # symbol: ⑂
     SYMBOL_GIT_MODIFIED='*'
-    SYMBOL_GIT_PUSH='↑'
-    SYMBOL_GIT_PULL='↓'
-
-    if [[ -z "$PS_SYMBOL" ]]; then
-      case "$(uname)" in
-          Darwin)   PS_SYMBOL='';;
-          Linux)    PS_SYMBOL='$';;
-          *)        PS_SYMBOL='%';;
-      esac
-    fi
-
-    __git_info() {
-        [[ $POWERLINE_GIT = 0 ]] && return # disabled
-        hash git 2>/dev/null || return # git not found
-        local git_eng="env LANG=C git"   # force git output in English to make our work easier
-
-        # get current branch name
-        local ref=$($git_eng symbolic-ref --short HEAD 2>/dev/null)
-
-        if [[ -n "$ref" ]]; then
-            # prepend branch symbol
-            ref=$SYMBOL_GIT_BRANCH$ref
-        else
-            # get tag name or short unique hash
-            ref=$($git_eng describe --tags --always 2>/dev/null)
-        fi
-
-        [[ -n "$ref" ]] || return  # not a git repo
-
-        local marks
-
-        # scan first two lines of output from `git status`
-        while IFS= read -r line; do
-            if [[ $line =~ ^## ]]; then # header line
-                [[ $line =~ ahead\ ([0-9]+) ]] && marks+=" $SYMBOL_GIT_PUSH${BASH_REMATCH[1]}"
-                [[ $line =~ behind\ ([0-9]+) ]] && marks+=" $SYMBOL_GIT_PULL${BASH_REMATCH[1]}"
-            else # branch is modified if output contains more lines after the header line
-                marks="$SYMBOL_GIT_MODIFIED$marks"
-                break
-            fi
-        done < <($git_eng status --porcelain --branch 2>/dev/null)  # note the space between the two <
-
-        # print the git branch segment without a trailing newline
-        printf " $ref$marks"
-    }
+    SYMBOL_GIT_PUSH='\342\[\206\221\]' # symbol: ↑
+    SYMBOL_GIT_PULL='\342\[\206\223\]' # symbol: ↓
+    SYMBOL_VIRTUAL_ENV='\342\[\210\207\]' # symbol: ∇
+    SYMBOL_APPLE='\357\[\243\277\]' # symbol: 
 
     ps1() {
         # Check the exit code of the previous command and display different
         # colors in the prompt accordingly.
         if [ $? -eq 0 ]; then
-            local symbol="$COLOR_SUCCESS $PS_SYMBOL $RESET"
+            local symbol="$COLOR_SUCCESS $SYMBOL_APPLE $RESET"
         else
-            local symbol="$COLOR_FAILURE $PS_SYMBOL $RESET"
+            local symbol="$COLOR_FAILURE $SYMBOL_APPLE $RESET"
         fi
 
         local cwd="$COLOR_CWD\W$RESET"
@@ -74,22 +33,45 @@ __powerline() {
         if [ -z $VIRTUAL_ENV ]; then
             local virtualenv=""
         else
-            local virtualenv=" $COLOR_VIRTUAL_ENV∇$(basename $VIRTUAL_ENV)$RESET"
-        fi
-        # Bash by default expands the content of PS1 unless promptvars is disabled.
-        # We must use another layer of reference to prevent expanding any user
-        # provided strings, which would cause security issues.
-        # POC: https://github.com/njhartwell/pw3nage
-        # Related fix in git-bash: https://github.com/git/git/blob/9d77b0405ce6b471cb5ce3a904368fc25e55643d/contrib/completion/git-prompt.sh#L324
-        if shopt -q promptvars; then
-            __powerline_git_info="$(__git_info)"
-            local git="$COLOR_GIT\${__powerline_git_info}$RESET"
-        else
-            # promptvars is disabled. Avoid creating unnecessary env var.
-            local git="$COLOR_GIT$(__git_info)$RESET"
+            local virtualenv=" $COLOR_VIRTUAL_ENV$SYMBOL_VIRTUAL_ENV$(basename $VIRTUAL_ENV)$RESET"
         fi
 
-        PS1="$cwd$git$virtualenv$symbol"
+        # Fetch the git info
+        local git_eng="env LANG=C git"   # force git output in English to make our work easier
+
+        # get current branch name
+        local ref=$($git_eng symbolic-ref --short HEAD 2>/dev/null)
+
+        if [[ -n "$ref" ]]; then
+            # prepend branch symbol
+            ref="$SYMBOL_GIT_BRANCH$ref"
+        else
+            # get tag name or short unique hash
+            ref="$($git_eng describe --tags --always 2>/dev/null)"
+        fi
+
+        local git=""
+
+        if [[ -n "$ref" ]]; then
+
+            local marks
+
+            # scan first two lines of output from `git status`
+            while IFS= read -r line; do
+                if [[ $line =~ ^## ]]; then # header line
+                    [[ $line =~ ahead\ ([0-9]+) ]] && marks+=" $SYMBOL_GIT_PUSH${BASH_REMATCH[1]}"
+                    [[ $line =~ behind\ ([0-9]+) ]] && marks+=" $SYMBOL_GIT_PULL${BASH_REMATCH[1]}"
+                else # branch is modified if output contains more lines after the header line
+                    marks="$SYMBOL_GIT_MODIFIED$marks"
+                    break
+                fi
+            done < <($git_eng status --porcelain --branch 2>/dev/null)  # note the space between the two <
+
+            # print the git branch segment without a trailing newline
+            local git=" $COLOR_GIT$ref$marks$RESET"
+        fi
+
+        PS1="$cwd$virtualenv$git$symbol"
     }
 
     PROMPT_COMMAND="ps1${PROMPT_COMMAND:+; $PROMPT_COMMAND}"
